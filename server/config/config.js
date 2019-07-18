557aca773479a88ba300403b836d272efc34606a
Ramiro <msr.ramiro@gmail.com>
2019-07-17 22:26:52 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
 //========================
 //Puerto
 //========================
-process.env.NODE_ENV = process.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //Conexion
 let urlDB;
-if ( process.env.NODE_ENV == 'dev'){
+if ( process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = 'mongodb+srv://strider:hcRDnmNa6zwTEVQb@cluster0-aw81m.mongodb.net/cafe';
