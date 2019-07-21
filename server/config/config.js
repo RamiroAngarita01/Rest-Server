eef861e41d32c13e0053d447e55b7c3833145585
Ramiro <msr.ramiro@gmail.com>
2019-07-21 00:35:15 -0500

    Variable de entorno Mongo prueba 2

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:hcRDnmNa6zwTEVQb@cluster0-aw81m.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI; 
 }
 process.env.urlDB = urlDB;
 //========================
