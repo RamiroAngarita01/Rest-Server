f8157ed4d336929586d90d7c7011d15e5afc9455
Ramiro <msr.ramiro@gmail.com>
2019-07-21 00:28:18 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb+srv://strider:hcRDnmNa6zwTEVQb@cluster0-aw81m.mongodb.net/cafe';
 }
 process.env.urlDB = urlDB;
 //========================
